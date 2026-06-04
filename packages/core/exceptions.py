9be08d17a52d95ff579ba6b3001a0d0c4037025b
Sanjay G <gokulesh2615@gmail.com>
2026-06-04 15:23:32 +0530

    feat(errors): implement typed exception hierarchy

---
+++ packages/core/exceptions.py
@@ -0,0 +1,13 @@
+class OrchestrusError(Exception):
+    """Base exception class for all orchestrus exceptions."""
+
+    def __init__(self, message: str):
+        self.message: str = message
+        super().__init__(message)
+
+
+class OrchestrationError(OrchestrusError):
+    """Raised when there is an error during orchestration."""
+
+    def __init__(self, message: str):
+        super().__init__(message)
